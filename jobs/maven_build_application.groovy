c7a508329ec0338a20fbc5479dd2280db5cadeca
Utkarsh Srivastava <utkarshsri0701@gmail.com>
2022-06-03 15:07:37 +0530

    updated seed files
    updated seed files

--- jobs/maven_build_application.groovy
+++ jobs/maven_build_application.groovy
@@ -1,36 +1,33 @@
 jobs('Spring-application-maven-seed-build') {
 
-    definition {
-           cps {
-             script('''
-    tools {
-        maven 'maven3.8.5'
-    }
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Build Application started'
-                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/opstree/spring3hibernate.git']]])
-                sh "mvn -f pom.xml clean validate test compile" 
+definition {
+    cps {
+        script('''
+            tools {
+                maven 'maven3.8.5'
             }
-        }
-        stage('Test') {
-            steps {
-                echo 'Test Application'
+            stages {
+                stage('Build') {
+                steps {
+                    echo 'Build Application started'
+                    echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/opstree/spring3hibernate.git']]])
+                    sh "mvn -f pom.xml clean validate test compile" 
+                }
             }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploy Application'
+            stage('Test') {
+                steps {
+                    echo 'Test Application'
+                }
             }
-        }     
-    }    
-    
+            stage('Deploy') {
+                steps {
+                    echo 'Deploy Application'
+                }
+            }     
+        }    
        '''.stripIndent())
        sandbox()
-          }
-      }
-  }
+        }
+    }
 }
