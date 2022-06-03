9eec2bb346140d5af116714bccaababca900c884
Utkarsh Srivastava <utkarshsri0701@gmail.com>
2022-06-03 14:23:32 +0530

    changes in seed file
    changes in seed file

--- jobs/maven_build_application.groovy
+++ jobs/maven_build_application.groovy
@@ -1,3 +1,36 @@
-jobs{
+jobs('Spring-application-maven-seed-build') {
 
+    definition {
+           cps {
+             script('''
+    tools {
+        maven 'maven3.8.5'
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Build Application started'
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/opstree/spring3hibernate.git']]])
+                sh "mvn -f pom.xml clean validate test compile" 
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Test Application'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploy Application'
+            }
+        }     
+    }    
+    
+       '''.stripIndent())
+       sandbox()
+          }
+      }
+  }
 }
